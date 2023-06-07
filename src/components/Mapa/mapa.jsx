c206266d8550d19ed029586554accdedcf9d2925
Johana Pokorná <johanapokorna@Johana-MacBook-Air.local>
2023-06-07 16:28:31 +0200

    první pokus napojení map

--- src/components/Mapa/mapa.jsx
+++ src/components/Mapa/mapa.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './mapa.css';
 import { Bar } from '../Bar/bar';
+import {
+  Map,
+  ZoomControl,
+  MouseControl,
+  KeyboardControl,
+  CompassControl,
+  SyncControl,
+} from 'react-mapycz';
 
 export const Mapa = () => {
   return (
     <div className="map">
+      <Map height="400px" center={{ lat, lng }} zoom={zoom}>
+        <ZoomControl />
+        <CompassControl top={50} />
+        <KeyboardControl />
+        <MouseControl pan={true} wheel={true} zoom={true} />
+        <SyncControl />
+      </Map>
       <Bar />
     </div>
   );
