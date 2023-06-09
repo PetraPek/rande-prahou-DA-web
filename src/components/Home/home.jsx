8b9f04b3978f2527afecf06e969106127c6951c7
Johana Pokorná <johanapokorna@Johana-MacBook-Air.local>
2023-06-09 20:40:19 +0200

    stylování úvodní stránky

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -4,48 +4,46 @@ import figma_mapa_druhastrana from './img/figma_mapa_druhastrana.png';
 
 export const Home = () => {
   return (
-    <>
-      <div className="background">
-        <div className="content">
-          <div className="description">
+    <div className="content">
+      <div className="description_card">
+        <div className="description">
+          <p className="description__question">
+            NEVÍTE, KAM VZÍT SVŮJ NOVÝ OBJEV?
             <p className="description__question">
-              NEVÍTE, KAM VZÍT SVŮJ NOVÝ OBJEV?
-              <p className="description__question">
-                NEBO KAM VZÍT SVÉHO DLOUHOLETÉHO PARTNERA?
-              </p>
+              NEBO KAM VZÍT SVÉHO DLOUHOLETÉHO PARTNERA?
             </p>
-            <br />
-            <p className="description__introduction">
-              Překvapte svoji drahou polovičku a vyražte na námi doporučená
-              místa. Nikdo nemusí vědět, že jsme vám pomohli právě my. My to na
-              vás neřekneme.
-            </p>
-            <br />
-            <p className="description__project">
-              Náš projekt má za cíl vás dostat na místa, která jste třeba dosud
-              neznali. Nevycházíme z žádného obecného doporučení. Vycházíme z
-              vlastních zkušeností s každým místem.
-            </p>
+          </p>
+          <br />
+          <p className="description__introduction">
+            Překvapte svoji drahou polovičku a vyražte na námi doporučená místa.
+            Nikdo nemusí vědět, že jsme vám pomohli právě my. My to na vás
+            neřekneme.
+          </p>
+          <br />
+          <p className="description__project">
+            Náš projekt má za cíl vás dostat na místa, která jste třeba dosud
+            neznali. Nevycházíme z žádného obecného doporučení. Vycházíme z
+            vlastních zkušeností s každým místem.
+          </p>
 
-            <h3>Grafický návrh</h3>
-            <div className="figma">
-              <img
-                className="figma_logo"
-                src={figma_logo_web}
-                alt="figma_logo_web"
-              />
-              <img
-                className="figma_secondpage"
-                src={figma_mapa_druhastrana}
-                alt="figma-mapa-druhastrana"
-              />
-            </div>
-            <a class="whereToGo" href="/mapa">
-              Kam vyrazit?
-            </a>
+          <h3>Grafický návrh</h3>
+          <div className="figma">
+            <img
+              className="figma_picture"
+              src={figma_logo_web}
+              alt="figma_logo_web"
+            />
+            <img
+              className="figma_picture"
+              src={figma_mapa_druhastrana}
+              alt="figma-mapa-druhastrana"
+            />
           </div>
+          <a class="whereToGo" href="/mapa">
+            Kam vyrazit?
+          </a>
         </div>
       </div>
-    </>
+    </div>
   );
 };
