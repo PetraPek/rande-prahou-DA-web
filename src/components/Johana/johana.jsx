f45f1f70c7481f10215d8af1a229d4168944384e
Johana Pokorná <johanapokorna@Johana-MacBook-Air.local>
2023-06-09 22:28:39 +0200

    stylování osobních stránek

--- src/components/Johana/johana.jsx
+++ src/components/Johana/johana.jsx
@@ -4,13 +4,13 @@ import johyy from './img/johyy.jpg';
 
 export const Johana = () => {
   return (
-    <div className="content">
+    <div className="personal__content">
       <div className="personal__card">
+        <img className="personal__foto" src={johyy} alt="Johana Pokorná" />
         <div className="personal__description">
           <h1 className="personal__title">Johanka</h1>
-          <img className="personal__foto" src={johyy} alt="Johana Pokorná" />
 
-          <p className="personal__description">
+          <p className="personal__info">
             Kdo jsem a něco o mně - jsem otevřená kniha, miluju zábavu, ráda se
             pousměji, zasměji a nakonec i rozesměji. Ráda trávím čas s přáteli,
             ale zároveň potřebuji mít někdy vlastní klid, ač to na první pohled
@@ -18,13 +18,14 @@ export const Johana = () => {
             jednou využít i ve své profesi.
           </p>
 
-          <p className="personal__description">
+          <p className="personal__info">
             Co se týče mé cesty do IT - je tomu asi rok, kdy jsem se rozhodla o
             změnu kariérní cesty a vybrala jsem si právě IT. Googlila jsem a
             googlila, jak se prý v IT dělá často, a narazila na kurz Digitální
             akademie, pro kterou jsem se po aboslvování jiných kurzu od
             Czechitas, rozhodla snadno a rychle.
           </p>
+
           <a className="gitHub" href="https://github.com/pokornajohana">
             GitHub
           </a>
--- src/components/Petra/petra.jsx
+++ src/components/Petra/petra.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import './petra.css';
+import '../Johana/johana.css';
 import petra from './img/petra.jpg';
 
 export const Petra = () => {
   return (
-    <div className="content">
+    <div className="personal__content">
       <div className="personal__card">
+        <img className="personal__foto" src={petra} alt="Petra Pekařová" />
         <div className="personal__description">
           <h1 className="personal__title">Peťa</h1>
-          <img className="personal__foto" src={petra} alt="Petra Pekařová" />
 
-          <p className="personal__description">
+          <p className="personal__info">
             Ke kódování, programování a celkově k Czechitas jsem se dostala jako
             slepý k houslím díky své bývalé práci.
           </p>
 
-          <p className="personal__description">
+          <p className="personal__info">
             Působila jsem na pozici IT Recruitera a rozčilovalo mě, že nerozumím
             tomu, co kandidátům nabízím. Začala jsem googlit a našla Czechitas a
             jejich kurzy. Absolvovala jsem různé kurzy abych věděla co dělá
