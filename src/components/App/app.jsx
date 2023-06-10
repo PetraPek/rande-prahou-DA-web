c7f6055d7d14b9130cbed3dd190364e034795d6c
Johana Pokorná <johanapokorna@Johana-MacBook-Air.local>
2023-06-10 10:49:12 +0200

    nahrání nové mapy - mapbox

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import { Header } from '../Header/header';
 import { Home } from '../Home/home';
 import { Petra } from '../Petra/petra';
--- src/components/Mapa/mapa.jsx
+++ src/components/Mapa/mapa.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './mapa.css';
+import mapboxgl from '!mapbox-gl';
 import { Bar } from '../Bar/bar';
 import { SimpleMap } from '../SimpleMap/simpleMap';
 
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoiam9oYW5hcG9rb3JuYSIsImEiOiJjbGlwMTdleWMwNDF5M2dvNWc1NWwzNXprIn0.hEIF0JDNVVOQUXffkpgyxg';
+
 export const Mapa = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
+
   return (
     <div className="map">
-      <SimpleMap />
       <Bar />
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
 };
