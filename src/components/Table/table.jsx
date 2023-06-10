bfa5be6e0cad0eda831a2bab1b50f58ed7fb1c99
Johana Pokorná <johanapokorna@Johana-MacBook-Air.local>
2023-06-10 17:47:00 +0200

    stylování informativního okénka

--- src/components/Table/table.jsx
+++ src/components/Table/table.jsx
@@ -5,7 +5,31 @@ import { places } from '../../../places';
 export const Table = () => {
   return (
     <div className="table">
-      {/* <img src={() => places.map((onePlace) => onePlace)} /> */}
+      <div className="colour_part">
+        <span className="place_name">"restaurace"</span>
+        <span className="place_address">"adresa"</span>
+      </div>
+      <div className="opening_hours">
+        <table>
+          <ul>
+            <li>Po: </li>
+            <li>Út: </li>
+            <li>St: </li>
+            <li>Čt: </li>
+          </ul>
+        </table>
+        <table>
+          <ul>
+            <li>Pá: </li>
+            <li>So: </li>
+            <li>Ne: </li>
+          </ul>
+        </table>
+      </div>
+      {/* {places.map((place) => {
+        console.log(place);
+        return <a href={place.url} className="place_link" />;
+      })} */}
     </div>
   );
 };
