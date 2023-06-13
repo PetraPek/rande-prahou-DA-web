2858bd3e6d95d0400c5b37184d29bf187fbba331
Johana Pokorná <johanapokorna@Johana-MacBook-Air.local>
2023-06-13 17:55:34 +0200

    stylování boční lišty pro mobil + přidání filtrovacího tlačítka

--- src/components/Bar/bar.jsx
+++ src/components/Bar/bar.jsx
@@ -9,9 +9,20 @@ export const Bar = ({ onFilterChange }) => {
     setOpenBar(!openBar);
   };
   return (
-    <div className="bar">
-      <div className="filter__icon" onClick={handleClick}>
-        {/* <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAMVJREFUSEvtlFEOwyAIhuFm3W3wFOtOAbdZb+aiKUnTKehsuz3MNxP4P4FfEE4+eLI+XA9g5hkR7yOVxRgfIYQ5abxVICJxRFxziShrVwEa0AvTB34V8ASACQAWIrr1VCAiOdecQRLUwB5ILadq0x6IFWv+gxaIF+N+NEvAEy/atDTUktDmQ5pmcCtQ4N7f+3vNbX8AeCvlN1vEzBMiprVwfAVpfay7CrY75zAXrULLKp5g5umZQW5Jq7BSmwHeS4db9CngBUNtpRlLnXWHAAAAAElFTkSuQmCC" /> */}
+    <>
+      <svg
+        className="filter__icon"
+        onClick={handleClick}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        style={{ fill: 'rgba(108, 108, 108, 1)' }}
+      >
+        <path d="M21 3H5a1 1 0 0 0-1 1v2.59c0 .523.213 1.037.583 1.407L10 13.414V21a1.001 1.001 0 0 0 1.447.895l4-2c.339-.17.553-.516.553-.895v-5.586l5.417-5.417c.37-.37.583-.884.583-1.407V4a1 1 0 0 0-1-1zm-6.707 9.293A.996.996 0 0 0 14 13v5.382l-2 1V13a.996.996 0 0 0-.293-.707L6 6.59V5h14.001l.002 1.583-5.71 5.71z" />
+      </svg>
+
+      <div className={'bar__desktop ' + (openBar ? 'bar' : 'bar__off')}>
         <div className="filters">
           <p className="filters__title">Filtr</p>
           <br />
@@ -27,6 +38,6 @@ export const Bar = ({ onFilterChange }) => {
 
         {/* <button className="search">Vyhledat</button> */}
       </div>
-    </div>
+    </>
   );
 };
